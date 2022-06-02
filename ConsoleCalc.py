d23de1ebda7e25f45cb9c6c4edb4566de0d05203
ViolentCalm <106648572+ViolentCalm@users.noreply.github.com>
2022-06-01 18:14:23 -0700

    Alpha V1.1
    Added another level of calculation, now you can apply mathematical to the result of the first calculation, and if you select n or N you can exit.

--- ConsoleCalc.py
+++ ConsoleCalc.py
@@ -1,14 +1,23 @@
 import decimal
+import sys
 
+ 
+RESULT = decimal.Decimal()
+RESULT2 = decimal.Decimal()
 
-RESULT = decimal.Decimal("0")
+print("  Welcome to the most basic calculator ever!  ")
+print("00000000")
+print("0XX00XX0")
+print("0xx00xx0")
+print("00000000")
+print("0x0000x0")
+print("00x00x00")
+print("000xx000")
+print("00000000")
 
 fNumb = decimal.Decimal(input("  Enter First Number.  "))
-
 fNumbTwo = decimal.Decimal(input("  Enter Second Number  "))
-
 fOperand = str(input("  Enter Operation + - * /  "))
-
 
 if fOperand == "+":
     RESULT = (fNumb + fNumbTwo)  
@@ -19,7 +28,29 @@ if fOperand == "*":
 if fOperand == "/":
     RESULT = (fNumb / fNumbTwo)
 
-print("  " , RESULT)
+print(RESULT)
 
-input("  Press Enter To Exit")
+question = str(input("  Do you want to calculate this against a new number? Y or N  "))
+
+
+
+
+if question == str("y" or "Y"):
+    fNumbThree = decimal.Decimal(input("  Enter Third Number  "))
+    fOperand2 = str(input(" Enter Second Operation + - * /  "))
+    
+    if fOperand2 == "+":
+        RESULT2 = (RESULT + fNumbThree)  
+    if fOperand2 == "-":
+        RESULT2 =(RESULT - fNumbThree) 
+    if fOperand2 == "*":
+        RESULT2 = (RESULT * fNumbThree) 
+    if fOperand2 == "/":
+        RESULT2 = (RESULT / fNumbThree)
+
+print("  " , RESULT2)
+
+if question != "y" or "Y":
+    exit(" Have a nice day! ")
+
 
